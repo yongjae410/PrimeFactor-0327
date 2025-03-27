c71c3cbc6e546e70ab887f3608a3a49a375a232a
yongjae410 <yongjae410@gmail.com>
2025-03-27 14:31:15 +0900

    cycle 2 Green

--- prime_factor.cpp
+++ prime_factor.cpp
@@ -10,6 +10,10 @@ public:
 	vector<int> of(int num)
 	{
 		vector<int> result = { };
+		if (num > 1)
+		{
+			result.push_back(num);
+		}
 		return result;
 	}
 private:
--- prime_factor_test.cpp
+++ prime_factor_test.cpp
@@ -10,8 +10,15 @@ using namespace testing;
 TEST(PrimeFactorTestGroup, tc1)
 {
 	PrimeFactor pf;
-	vector<int> expected { };
+	vector<int> expected = { };
 	EXPECT_EQ(expected, pf.of(1));
+}
+
+TEST(PrimeFactorTestGroup, tc2)
+{
+	PrimeFactor pf;
+	vector<int> expected = { 2 };
+	EXPECT_EQ(expected, pf.of(2));
 }
 
 
